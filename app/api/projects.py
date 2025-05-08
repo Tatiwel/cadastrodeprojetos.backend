e1b3fc1444cdb0a6c13748f27f8a8ad8cc952b09
Tatiwel <danieldandez@gmail.com>
2025-05-08 10:55:46 -0300

    Structuring fix

---
+++ app/api/projects.py
@@ -0,0 +1,66 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.db.session import SessionLocal
+from app.models.project import Project
+from app.api.schemas import ProjectCreate, ProjectResponse, ProjectUpdate
+
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.get("/projects", response_model=list[ProjectResponse])
+def list_projects(db: Session = Depends(get_db)):
+    return db.query(Project).all()
+
+@router.get("/projects/{project_id}", response_model=ProjectResponse)
+def get_project(project_id: int, db: Session = Depends(get_db)):
+    project = db.query(Project).get(project_id)
+    if not project:
+        raise HTTPException(status_code=404, detail="Projeto não encontrado")
+    return project
+
+@router.post("/projects", response_model=ProjectResponse)
+def create_project(project: ProjectCreate, db: Session = Depends(get_db)):
+    db_project = Project(**project.model_dump())
+    db.add(db_project)
+    db.commit()
+    db.refresh(db_project)
+    return db_project
+
+
+@router.get("/projects-status")
+def count_project_status(db: Session = Depends(get_db)):
+    ativos = db.query(Project).filter_by(status="Ativo").count()
+    pausados = db.query(Project).filter_by(status="Pausado").count()
+    finalizados = db.query(Project).filter_by(status="Finalizado").count()
+
+    return {
+        "Ativo": ativos,
+        "Pausado": pausados,
+        "Finalizado": finalizados
+    }
+
+@router.put("/projects/{project_id}", response_model=ProjectResponse)
+def update_project(project_id: int, update: ProjectUpdate, db: Session = Depends(get_db)):
+    project = db.query(Project).get(project_id)
+    if not project:
+        raise HTTPException(status_code=404, detail="Projeto não encontrado")
+    for key, value in update.dict().items():
+        setattr(project, key, value)
+    db.commit()
+    db.refresh(project)
+    return project
+
+@router.delete("/projects/{project_id}")
+def delete_project(project_id: int, db: Session = Depends(get_db)):
+    project = db.query(Project).get(project_id)
+    if not project:
+        raise HTTPException(status_code=404, detail="Projeto não encontrado")
+    db.delete(project)
+    db.commit()
+    return {"ok": True}
--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
-from app.api.routes import router
+from app.api.routes import api_router
 from app.db.session import Base, engine
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)  # Cria tabelas se não existirem
 
-app.include_router(router)
+# Inclui as rotas da API
+app.include_router(api_router)
+
+# Endpoint de teste
+@app.get("/")
+def read_root():
+    return {"Hello": "API está ativa no ambiente;"}
